100dbbbbdcee9e446f0b4a36b1347f847818c948
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-03-28 20:08:41 -0500

    save

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac033451d39145bf2666dd97ce2af26fd304b384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893b6aea283216bf0c523fef9fc42a001cfda82b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
